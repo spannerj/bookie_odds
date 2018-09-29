181d82e0bb9cfa1efff8a98a05191901141f421f
spencer <spencer.jago@gmail.com>
2018-09-29 01:35:04 +0100

    remove test

--- clock.py
+++ clock.py
@@ -3,10 +3,10 @@ import datetime
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    print(datetime.datetime.now())
-    print('This job is run every minute.')
+# @sched.scheduled_job('interval', minutes=1)
+# def timed_job():
+#     print(datetime.datetime.now())
+#     print('This job is run every minute.')
 
 @sched.scheduled_job('cron', day_of_week='fri-sat', hour=1, minute=30)
 def scheduled_job():
