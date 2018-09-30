78395a3bee2eede222ddc344d15cfccc2978ea10
spencer <spencer.jago@gmail.com>
2018-09-30 08:41:54 +0100

    again

--- clock.py
+++ clock.py
@@ -9,15 +9,27 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    options = Options()  
-    options.add_argument("--headless")
-    # options.add_argument("--start-maximized")
-    options.add_argument("--window-size=1300,1000")
-    options.add_argument('--disable-gpu')
-    options.add_argument('--remote-debugging-port=9222')
-    options.add_argument('--no-sandbox')
-    options.binary_location = os.environ['GOOGLE_CHROME_BIN']
-    driver = webdriver.Chrome(options=options)
+    # options = Options()  
+    # options.add_argument("--headless")
+    # # options.add_argument("--start-maximized")
+    # options.add_argument("--window-size=1300,1000")
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--remote-debugging-port=9222')
+    # options.add_argument('--no-sandbox')
+    # options.binary_location = os.environ['GOOGLE_CHROME_BIN']
+    # driver = webdriver.Chrome(options=options)
+
+    CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+
+    chrome_options = webdriver.ChromeOptions()
+
+    chrome_options.binary_location = '.apt/usr/bin/google-chrome-stable'
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('headless')
+
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
+    print('browser is ready')    
 
     driver.get("http://www.python.org")
     print(datetime.datetime.now())
