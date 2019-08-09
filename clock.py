9c405ef0de7537a68e5c6b313c57a81470117dd9
spencerjago <spencer.jago@digital.landregistry.gov.uk>
2019-08-09 16:43:21 +0100

    add email functionality

--- clock.py
+++ clock.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-import sys
+import os
 import logging
+import yagmail
 from tasks.players import get_players
 
 logging.basicConfig(level=logging.INFO)
@@ -8,14 +9,22 @@ sched = BlockingScheduler()
 
 # @sched.scheduled_job('interval', seconds=5)
 # def timed_job():
-#     logging.info(' - Getting players')
-#     get_players()
+#     password = os.environ['PWORD']
+#     yag = yagmail.SMTP('spencer.jago@digital.landregistry.gov.uk', password)
+#     contents = [ 'Site up to date. ']
+
+#     yag.send('spencer.jago@gmail.com', 'LR Fantasy Football', contents)
 
 
 @sched.scheduled_job('cron', hour=1, minute=0)
 def daily_get_players():
     logging.info(' - Getting players')
     get_players()
+    
+    password = os.environ['PWORD']
+    yag = yagmail.SMTP('spencer.jago@digital.landregistry.gov.uk', password)
+    contents = [ 'Site up to date. ']
+    yag.send('spencer.jago@gmail.com', 'LR Fantasy Football', contents)
 
 logging.info(' - Schedule starting')
 sched.start()
