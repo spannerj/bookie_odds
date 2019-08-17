e8dddbe1e8c7153bc23c98b26aada03e3ce88f88
spencerjago <spencer.jago@digital.landregistry.gov.uk>
2019-08-17 02:26:43 +0100

    tweak schedule

--- clock.py
+++ clock.py
@@ -10,21 +10,18 @@ from apscheduler.triggers.combining import OrTrigger
 from apscheduler.triggers.cron import CronTrigger
 
 trigger = OrTrigger([
-#    CronTrigger(hour='16-23'),
-   CronTrigger(hour='02-03'),
-   CronTrigger(minute='*/1')
+   CronTrigger(hour='16-23'),
+#    CronTrigger(hour='02-03'),
+   CronTrigger(minute='*/15')
 ])
 
 
+@sched.scheduled_job('cron', minute='59', hour='19-23', month='1-5,8-12')
+def cron_get_players():
+    logging.info(' - Getting players')
+    get_players()
 
-# @sched.scheduled_job('cron', minute='59', hour='19-23', month='1-5,8-12')
-# def cron_get_players():
-#     logging.info(' - Getting players')
-#     get_players()
 
-
-# @sched.scheduled_job('cron', hour='16-23', minute='1')
-# @sched.scheduled_job('cron', minute='3', hour='2')
 def daily_get_selections():
     logging.info(' - Getting selections')
     get_selections()
