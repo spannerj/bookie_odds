be98903f284c6a3bff6ccfdbea3d2c13ddf0973d
spencer <spencer.jago@gmail.com>
2019-08-18 23:32:36 +0100

    change schedule

--- clock.py
+++ clock.py
@@ -8,10 +8,10 @@ from apscheduler.triggers.cron import CronTrigger
 logging.basicConfig(level=logging.INFO)
 sched = BlockingScheduler()
 
-trigger = AndTrigger([
-   CronTrigger(hour='8-23'),
-   CronTrigger(minute='*/15')
-])
+# trigger = AndTrigger([
+#    CronTrigger(hour='8-23'),
+#    CronTrigger(minute='*/15')
+# ])
 
 
 @sched.scheduled_job('cron', minute='59', hour='19-23', month='1-5,8-12')
@@ -20,11 +20,12 @@ def cron_get_players():
     get_players()
 
 
+@sched.scheduled_job('cron', minute='*/15', hour='8-23')
 def daily_get_selections():
     logging.info(' - Getting selections')
     get_selections()
 
 
 logging.info(' - Schedule starting')
-sched.add_job(daily_get_selections, trigger)
+# sched.add_job(daily_get_selections, trigger)
 sched.start()
