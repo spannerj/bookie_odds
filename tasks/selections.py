d3b83dae97ac987503fd10035dc21369a2516082
spencer <spencer.jago@gmail.com>
2019-08-24 01:40:57 +0100

    added all bet types

--- tasks/selections.py
+++ tasks/selections.py
@@ -10,12 +10,13 @@ import traceback
 import time
 
 
-URL_LIST = [
-    'https://betracingnationclub.com/daily-double/',
-    'https://betracingnationclub.com/selections/',
-    'https://betracingnationclub.com/a-petes-patent/',
-    'https://betracingnationclub.com/a-saturday-six-trebles/'
-]
+URL_DICT = {
+    "Double": "https://betracingnationclub.com/daily-double/",
+    "Singles": "https://betracingnationclub.com/selections/",
+    "Patent": "https://betracingnationclub.com/a-petes-patent/",
+    "Trebles": "https://betracingnationclub.com/a-saturday-six-trebles/"
+}
+
 
 def connect_to_db():
     try:
@@ -32,49 +33,60 @@ def commit_and_close(connection):
     connection.close()
 
 
-def get_db_hashes(type):
+def get_db_hash(bet_type):
 
     connection = connect_to_db()
     cursor = connection.cursor()
 
     select_sql = """
-                 SELECT bet_hash1,  bet_hash2, bet_hash3, bet_hash4, bet_hash5, bet_hash6
-                 FROM selections_test
+                 SELECT bet_hash
+                 FROM selections
                  WHERE bet_type = '{}'
                  fetch first 1 row only
                  """
 
-    cursor.execute(select_sql.format(type))
+    cursor.execute(select_sql.format(bet_type))
     result = cursor.fetchall()
     commit_and_close(connection)
-    return result[0]
+    return result[0][0]
 
 
-def insert_new_hashes(bet_list, bet_type):
+def xcheck(bet):
+    stripped_bet = "".join(bet.split())
+    if 'xxxxx' in stripped_bet:
+        return True
+    else:
+        return False
 
+
+def bet_list_length_check(bet_list):
+    if len(bet_list) < 6:
+        for i in range(6 - len(bet_list)):
+            bet_list.append('')
+
+    return bet_list
+
+
+def insert_new_hashes(bet_hash, bet_type):
     connection = connect_to_db()
     cursor = connection.cursor()
 
     update_sql = """
-                 UPDATE selections_test
-                 SET bet_hash1=%s, bet_hash2=%s, bet_hash3=%s, bet_hash4=%s, bet_hash5=%s, bet_hash6=%s
+                 UPDATE selections
+                 SET bet_hash=%s
                  where bet_type = %s;
                  """
     try:
-        cursor.execute(update_sql, (bet_list[0], bet_list[1], bet_list[2], bet_list[3], bet_list[4], bet_list[5], bet_type,))
+        cursor.execute(update_sql, (bet_hash, bet_type,))
     except Exception as e:
         logging.error(e)
     commit_and_close(connection)
 
 
-def hash_check(hashed_bet_list, db_hashes, bet_list):
+def hash_check(hashed_bet_list, db_hash):
     changes = False
-    for i in range(5):
-        if hashed_bet_list[i] != db_hashes[i]:
-            changes = True
-            if bet_list[i] != 'None':
-                logging.info(' - New bet found')
-                send_message(bet_list[i])
+    if hashed_bet_list != db_hash:
+        changes = True
 
     return changes
 
@@ -86,35 +98,40 @@ def hash_it(text):
 def send_message(message):
     url = 'https://api.telegram.org'
     url = url + '/bot810436987:AAESEw086nXGtqt_w9r09-By-5W2bt4fqbM/sendMessage'
-    url = url + '?chat_id=-1001190331415&text=%s'
-    # requests.get(url.format(message))
+    url = url + '?chat_id=-1001190331415&text={}'
+    # url = 'https://api.telegram.org'
+    # url = url + '/bot810436987:AAESEw086nXGtqt_w9r09-By-5W2bt4fqbM/sendMessage'
+    # url = url + '?chat_id=-1001365813396&text={}'
+
+    requests.get(url.format(message))
 
 
 def login_to_site():
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--proxy-server='direct://'")
-        chrome_options.add_argument("--proxy-bypass-list=*")
-        chrome_options.add_argument("--start-maximized")
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--proxy-server='direct://'")
+    chrome_options.add_argument("--proxy-bypass-list=*")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--ignore-certificate-errors')
 
-        browser = webdriver.Chrome(options=chrome_options)
-        # browser.get('https://betracingnationclub.com/log-in/')
-        browser.get('https://betracingnationclub.com/wp-login.php')
-        time.sleep(1)
-        browser.find_element_by_id('user_login').send_keys(os.environ['WP_USER'])
-        browser.find_element_by_id('user_pass').send_keys(os.environ['WP_PASSWORD'])
-        time.sleep(1)
-        browser.find_element_by_name('wp-submit').click()
-        time.sleep(1)
+    browser = webdriver.Chrome(options=chrome_options)
+    # browser.get('https://betracingnationclub.com/log-in/')
+    browser.get('https://betracingnationclub.com/wp-login.php')
+    time.sleep(1)
+    browser.find_element_by_id('user_login').send_keys(os.environ['WP_USER'])
+    browser.find_element_by_id('user_pass').send_keys(os.environ['WP_PASSWORD'])
+    time.sleep(1)
+    browser.find_element_by_name('wp-submit').click()
+    time.sleep(1)
 
-        return browser
+    return browser
+
 
 def parse_table(browser, url):
     browser.get(url)
@@ -130,29 +147,49 @@ def parse_table(browser, url):
         for i in td:
             bet = bet + ' ' + i.text
 
-        if bet != 'None':
-            bet_list.append(bet.strip())  
-    return bet_list 
+        if bet.strip() != 'None':
+            bet_list.append(bet.strip())
+        else:
+            bet_list.append('')
+
+    return bet_list
+
+
+def assemble_bets(bet_type, bet_list):
+    message = ''
+    for i in range(len(bet_list)):
+        if bet_list[i] != '':
+            if not xcheck(bet_list[i]):
+                message = message + bet_list[i] + '\n'
+
+    if message.strip() != '':
+        message = bet_type + '\n' + message
+
+    return message
 
 
 def get_selections():
     try:
         browser = login_to_site()
-        for url in URL_LIST:
+        for bet_type, url in URL_DICT.items():
             bet_list = parse_table(browser, url)
-            print(bet_list)
 
-        hashed_bet_list = []
-        for bet in bet_list:
-            hashed_bet = hash_it(bet)
-            hashed_bet_list.append(hashed_bet)
+            bet_list = bet_list_length_check(bet_list)
+            message = assemble_bets(bet_type, bet_list)
 
-        db_hashes = get_db_hashes('Singles')
+            hashed_bet_list = hash_it(message)
 
-        if hash_check(hashed_bet_list, db_hashes, bet_list):
-            insert_new_hashes(hashed_bet_list, 'Singles')
-        else:
-            logging.info(' - No new bets.')
+            db_hash = get_db_hash(bet_type)
+
+            if hash_check(hashed_bet_list, db_hash):
+                if message != '':
+                    send_message(message)
+                    insert_new_hashes(hashed_bet_list, bet_type)
+                    logging.info(' - New ' + bet_type + ' bet found.')
+                else:
+                    logging.info(' - No new ' + bet_type + ' bets.')
+            else:
+                logging.info(' - No new ' + bet_type + ' bets.')
 
     except Exception as e:
         traceback.print_exc()
